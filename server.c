17e7b760f3ab31ea48ace1ec4d6312b1c7972c1b
Mihir Shete <mihirsht@gmail.com>
2016-01-05 03:19:28 -0600

    speedtest: add concurrency to support many clients at once

--- server.c
+++ server.c
@@ -7,11 +7,18 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
+#include <pthread.h>
 #include "packet.h"
 #include "common.h"
 
 #define DEFAULT_PORT 9000
 #define BACKLOG 50
+#define MAX_THREADS 2
+
+pthread_mutex_t lock;
+pthread_cond_t fill, empty;
+int volatile produce_i, consume_i, count;
+int tbuf[MAX_THREADS];
 
 int send_ack(int confd) {
     char *buffer;
@@ -69,78 +76,99 @@ void send_summarystats(int confd, char *s) {
     }
 }
 
-void process_ctr_msg(int confd) {
+void *process_ctr_msg(void *pconfd) {
+    int confd;
     struct timeval tv;
-    char *buf = malloc(RX_BUFSIZE);
+    char *buf;
     char *s = NULL;
     int cum_len = 0;
 
-    if (buf == NULL) {
-        printf("Cannot allocate memory for Rx\n");
-        exit(1);
-    }
-
-    tv.tv_sec = 60;
-    tv.tv_usec = 0;
-    if (setsockopt(confd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
-        perror("Error");
-    }
-
-    printf("Waiting for Client...\n");
     while (1) {
-        int len;
-        struct packet_header *hdr;
+        pthread_mutex_lock(&lock);
+        while(count == 0)
+            pthread_cond_wait(&fill, &lock);
+        //get connfd from buffer
+        confd = tbuf[consume_i];
+        consume_i = (consume_i + 1) % MAX_THREADS;
+        count--;
 
-        len = recv(confd, &buf[cum_len], RX_BUFSIZE - cum_len, 0);
-        if (len == -1) {
-            if (errno == EAGAIN) {
-                printf("Timed out waiting for commands from client\n");
+        pthread_cond_signal(&empty);
+        pthread_mutex_unlock(&lock);
+
+        buf = malloc(RX_BUFSIZE);
+        if (buf == NULL) {
+            printf("Cannot allocate memory for Rx\n");
+            continue;
+        }
+
+        tv.tv_sec = 60;
+        tv.tv_usec = 0;
+        if (setsockopt(confd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+            perror("Error");
+        }
+
+        printf("Waiting for Client...\n");
+        while (1) {
+            int len;
+            struct packet_header *hdr;
+
+            len = recv(confd, &buf[cum_len], RX_BUFSIZE - cum_len, 0);
+            if (len == -1) {
+                if (errno == EAGAIN) {
+                    printf("Timed out waiting for commands from client\n");
+                    close(confd);
+                    break;
+                }
+                continue;
+            } else if (len == 0) {
+                printf("Connection Closed\n");
                 close(confd);
                 break;
             }
-            continue;
-        } else if (len == 0) {
-            printf("Connection Closed\n");
-            close(confd);
-            break;
+
+            cum_len += len;
+            if (cum_len < RX_BUFSIZE)
+                continue;
+            cum_len = 0;
+
+            hdr = (struct packet_header *)buf;
+            
+            printf("Received Control Message: %c\n", hdr->type);
+            switch (hdr->type) {
+                case T_UPLOAD_TEST:
+                    printf("Ready for Upload Test...\n");
+                    if (send_ack(confd))
+                        s = run_rx_test(confd, 1);
+                    if (s != NULL) {
+                        send_summarystats(confd, s);
+                        free(s);
+                    } else {
+                        printf("Error! will not send summary stats\n");
+                    }
+                break;
+
+                case T_DOWNLOAD_TEST:
+                    printf("Ready for Download Test...\n");
+                    if (send_ack(confd))
+                        run_tx_test(confd);
+                break;
+            }
         }
 
-        cum_len += len;
-        if (cum_len < RX_BUFSIZE)
-            continue;
-        cum_len = 0;
-
-        hdr = (struct packet_header *)buf;
-        
-        printf("Received Control Message: %c\n", hdr->type);
-        switch (hdr->type) {
-            case T_UPLOAD_TEST:
-                printf("Ready for Upload Test...\n");
-                if (send_ack(confd))
-                    s = run_rx_test(confd, 1);
-                if (s != NULL) {
-                    send_summarystats(confd, s);
-                    free(s);
-                } else {
-                    printf("Error! will not send summary stats\n");
-                }
-            break;
-
-            case T_DOWNLOAD_TEST:
-                printf("Ready for Download Test...\n");
-                if (send_ack(confd))
-                    run_tx_test(confd);
-            break;
-        }
+        free(buf);
     }
-
-    free(buf);
 }
 
 void s_run(unsigned int port) {
     int fd;
     struct sockaddr_in my;
     int flag = 1;
+    pthread_t server_thread[MAX_THREADS];
+    int confd;
+
+	pthread_mutex_init(&lock, NULL);
+	pthread_cond_init(&fill, NULL);
+	pthread_cond_init(&empty, NULL);
 
     fd = socket(PF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
@@ -173,23 +201,38 @@ void s_run(unsigned int port) {
         return;
     }
 
+    int i;
+    for (i = 0; i < MAX_THREADS; i++) {
+        pthread_create(&server_thread[i], NULL,  process_ctr_msg, NULL);
+    }
+
     while (1) {
         struct sockaddr_storage their;
         socklen_t sz = sizeof(their);
-        int confd = accept(fd, (struct sockaddr *)&their, &sz);
+        confd = accept(fd, (struct sockaddr *)&their, &sz);
 
         if (confd == -1) {
             perror("cannot accept connection");
-            return;
+            continue;
         }
 
         if (setsockopt(confd, IPPROTO_TCP, TCP_NODELAY, (char *)&flag,
                             sizeof(int)) == -1) {
-            printf("Cannot disable Nagle! Exit\n");
-            exit(1);
+            printf("Cannot disable Nagle! Close\n");
+            close(confd);
+            continue;
         }
 
-        process_ctr_msg(confd);
+		pthread_mutex_lock(&lock);
+		while(count == MAX_THREADS)
+			pthread_cond_wait(&empty, &lock);
+		tbuf[produce_i] = confd;
+		produce_i = (produce_i + 1) % MAX_THREADS;
+		count++;
+
+		pthread_cond_signal(&fill);
+		pthread_mutex_unlock(&lock);
+
     }
 
     close(fd);
